aab0501ac56c8ca5ff7e28a08500b172934bd18f
Christoph Willinger <christoph.willinger@gmail.com>
2015-03-12 10:59:57 +0100

    Success

--- app.py
+++ app.py
@@ -1,11 +1,46 @@
 #!./env/bin/python3
+import datetime
+import urllib.request
+from bs4 import BeautifulSoup
 from flask import Flask
+from flask import render_template
+
+OCAT_URL = "https://overclockers.at"
 
 app = Flask(__name__)
 
+
+def get_ocat_html():
+    response = urllib.request.urlopen(OCAT_URL)
+    return response.read()
+
+
+def parse_news():
+    html = BeautifulSoup(get_ocat_html())
+    news = []
+
+    for item in html.find_all(class_="news"):
+        h2 = item.find("h2")
+        a = h2.find("a")
+        details = item.find(class_='details')
+        author = details.contents[2].strip()
+        date_string = details.contents[4].strip()
+
+        news.append({
+            'title': a.string,
+            'link': OCAT_URL + a.get('href'),
+            'description': item.find(class_='previewtext').text.strip(),
+            'author': author,
+            'date': date_string,
+        })
+
+    return news
+
+
 @app.route('/')
 def index():
-    return 'Hello oc.at!'
+    news = parse_news()
+    return render_template('rss2.0.xml', news=news)
 
 
 if __name__ == '__main__':
